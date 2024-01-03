2c614a62713fbbdcb2b4183e3f9d74fbef1f5fd7
Atom <ramirezatom@gmail.com>
2024-01-02 20:59:12 -0800

    Update photo_finder.py
    added option parsing code

--- photo_finder.py
+++ photo_finder.py
@@ -1,3 +1,24 @@
 # -*- coding: utf-8 -*-
+import argparse
+
+
+
+
 def main():
-    pass
+   # TODO: Can change later
+    parser = argparse.ArgumentParser(description = "Photo finder") 
+    
+    # setting up the options : photodir, help, and build index 
+    parser.add_argument("--photo_dir", type = str, required = True, 
+                        help = "photo_directory" )
+    
+    parser.add_argument("--h", help = "display help" )
+    
+    parser.add_argument("--build_index", help = "build index from scratch" )
+    
+    args = parser.parse_args()
+
+
+
+if __name__ == ' __main__':
+    main()
