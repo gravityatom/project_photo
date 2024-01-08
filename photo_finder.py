fcd5634dd3ded7855fda5ca21f9a80e1b5cc9cd6
Atom <ramirezatom@gmail.com>
2024-01-07 22:44:35 -0800

    updated script
    -Cleaned up some of the code
    -updated gitignore to ignore .csv files
    -added new code to extract and save metadata to csv

--- photo_finder.py
+++ photo_finder.py
@@ -1,13 +1,12 @@
 # -*- coding: utf-8 -*-
 import argparse
-
 import pathlib
-
 from exif import Image
 
+args = ' '
 
 def find_photos(photo_dir):
-   
+    global args
     photo_count = 0 
    
     csv_content = "photo,make,datetime,model\n"
@@ -22,46 +21,45 @@ def find_photos(photo_dir):
         
         photo_count += 1
        
-        
-       
         photo_metadata = extract_metadata(photo)
         
         # append row to csv_content
         csv_content += photo_metadata
         
-        
-
     # saving csv_content to a file
+    print(args.photo_db)
     f = open(args.photo_db, "w")
     f.write(csv_content)
     f.close()
         
-        
-        
     print(f"Photo count: {photo_count}.")
         
-
 
 def extract_metadata(photo):
     
     print(f"Extracting metadata from {photo}.")
        
    
-    # opens image
-    #image = Image.open(photo)
+   
     with open(photo, 'rb') as img_file:
         img = Image(img_file)
         
-    
-    csv_row = f"{photo},{img.get("make")},{img.get("datetime")},{img.get("model")}\n"
-    return csv_row
-    #print(f"{img.list_all()}")
 
+    print(f"make:{img.get('make')}")
+     
     
-    
+    csv_row = f'{photo},'
+    csv_row += f'{img.get("make")}'
+    csv_row += f'{img.get("datetime")}'
+    csv_row += f'{img.get("model")}'
+    csv_row += f'\n'
+
+   
+    return csv_row
 
 
 def main():
+    global args
     # TODO: Can change later
     parser = argparse.ArgumentParser(description = "Photo finder") 
     
