90560cde53b20b504c60167b7cbfc37838288d53
Atom <ramirezatom@gmail.com>
2024-01-08 21:50:09 -0800

    Update search_photos.py
    implemented search photos

--- search_photos.py
+++ search_photos.py
@@ -1,14 +1,34 @@
 # -*- coding: utf-8 -*-
 import argparse
-import pathlib
-
+import pandas as pd
 
 args = ' '
+
+def search_photos(photo_db):   
+    global args
+   
+    print("Inside search photos")
+   
+    df = pd.read_csv(photo_db)
+    
+    df.query('datetime == args.start_date' and 'datetime == args.end_date', inplace = True)
+    print(df)
+
+
+
+
+
+
+
+
+
+
+
 
 
 
 def main():
-    
+    global args
     
     # TODO: Can change later
     parser = argparse.ArgumentParser(description = "Search Photos") 
@@ -20,6 +40,9 @@ def main():
     parser.add_argument("--end_date", type = str, required = True, 
                         help = "end date" )
     
+    parser.add_argument("--photo_db", type = str, required = True, 
+                        help = "photo database in csv format" )
+   
     parser.add_argument("--h", help = "display help", action = "store_true")
     
     
@@ -27,7 +50,7 @@ def main():
 
     print(f"Searching images from {args.start_date} to {args.end_date}.")
     
-    
+    search_photos(args.photo_db)
     
 if __name__ == '__main__':
     
