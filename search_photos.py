1e084609305a1546ebde037c799bd9b9b481e52c
Carlos Ramirez <carlos@quarklogic.com>
2024-01-08 21:07:47 -0800

    Removed extraneous code
    Fixed code by removing unused code

--- search_photos.py
+++ search_photos.py
@@ -6,54 +6,6 @@ import pathlib
 args = ' '
 
 
-    global args
-    photo_count = 0 
-   
-    csv_content = "photo,make,datetime,model\n"
-   
-    print(f"Searching for photos in {photo_dir}.")
-    
-    start_dir = pathlib.Path(photo_dir)
-   
-    for photo in start_dir.rglob("*.jpg"):
-        
-        print(f"Found photo: {photo}")
-        
-        photo_count += 1
-       
-        photo_metadata = extract_metadata(photo)
-        
-        # append row to csv_content
-        csv_content += photo_metadata
-        
-    # saving csv_content to a file
-    print(args.photo_db)
-    f = open(args.photo_db, "w")
-    f.write(csv_content)
-    f.close()
-        
-    print(f"Photo count: {photo_count}.
-    
-    print(f"Extracting metadata from {photo}.")
-       
-   
-   
-    with open(photo, 'rb') as img_file:
-        img = Image(img_file)
-        
-
-    print(f"make:{img.get('make')}")
-     
-    
-    csv_row = f'{photo},'
-    csv_row += f'{img.get("make")}'
-    csv_row += f'{img.get("datetime")}'
-    csv_row += f'{img.get("model")}'
-    csv_row += "\n"
-
-   
-    return csv_row
-
 
 def main():
     
@@ -75,7 +27,6 @@ def main():
 
     print(f"Searching images from {args.start_date} to {args.end_date}.")
     
-    find_photos(args.photo_dir)
     
     
 if __name__ == '__main__':
